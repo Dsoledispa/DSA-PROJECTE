23748cfbd21728c90f9d1479ba7945ecf78d6867
Diego Soledispa <59051689+Dsoledispa@users.noreply.github.com>
2025-05-25 22:52:58 +0200

    swagger funcional

--- src/main/java/edu/upc/dsa/config/SwaggerConfiguration.java
+++ src/main/java/edu/upc/dsa/config/SwaggerConfiguration.java
@@ -1,30 +1,19 @@
 package edu.upc.dsa.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.apache.log4j.Logger;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
-import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Provider;
-import java.util.Collections;
-
-@Provider
-public class SwaggerConfiguration implements ContextResolver<OpenAPI> {
-    final static Logger logger = Logger.getLogger(SwaggerConfiguration.class);
-
-
-    @Override
-    public OpenAPI getContext(Class<?> type) {
-        logger.info("Funcionas");
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Tu API")
-                        .version("1.0.0")
-                        .description("Documentación generada con Swagger"))
-                .servers(Collections.singletonList(
-                        new Server().url("http://localhost:8080/dsaApp")
-
-                ));
-    }
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Tu API",
+                version = "1.0.0",
+                description = "Documentación generada con Swagger"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080/dsaApp")
+        }
+)
+public class SwaggerConfiguration {
+    // No hace falta nada más aquí
 }
